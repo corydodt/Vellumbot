3c66f563f6dfa2cc6b236ffc5809b45f2bfe7961
Cory Dodt <corymercurial@spam.goonmill.org>
2009-02-05 13:47:18 -0800

    tested linesyntax

---
+++ vellumbot/server/linesyntax.py
@@ -0,0 +1,254 @@
+r"""Define the syntax for parsing statements on IRC.
+Lines may contain the following syntax:
+    - A message that begins with the bot's name or begins with a dot
+      is a structured command (beginning on the first token after the bot's
+      name, if present), and it may take arguments.
+    - A single word beginning with a letter and prefixed by a * anywhere in
+      the line is the name of an actor.  There may be only one at most.
+    - An expression inside brackets [] is a verb.
+    - A verb starts with zero or more verbnames, and ends with an optional dice
+      expression.
+    - A dice expression
+    - A target starts with @ and there may be more than one.
+"""
+
+import string
+
+import re
+import shlex
+
+from simpleparse import parser, dispatchprocessor as disp
+from simpleparse.error import ParserSyntaxError
+from simpleparse.common import numbers
+from playtools import diceparser
+
+grammar = ( # {{{
+r'''# irc bot commands and recognized sentences
+<ws>                    := [ \t]*
+<identifier>            := [a-zA-Z], [a-zA-Z0-9_]*
+
+<commandLeader>         := '.'
+commandIdentifier       := identifier
+
+<botname>               := identifier
+
+<hail>                  := botname, ws, (':'/','), ws
+
+<CHAR>                  := -[[]/'['  
+
+commandArgs             := CHAR+ 
+
+command                 := (hail/commandLeader), ws, !, commandIdentifier, ([ \t]+, commandArgs)?, ws
+
+commandRoot             := ws,command
+
+
+
+<word>                  := [a-zA-Z0-9{}\/';":.,!@#$%^&*()-=_+]+  
+nonDiceWords            := (?-(diceExpression),word,ws)+  
+
+>diceExpressionAlone<   := diceExpression
+>aliasAlone<            := nonDiceWords
+>aliasPlusModifier<     := nonDiceWords, dieModifier
+>aliasPlusExpression<   := nonDiceWords, diceExpression  
+
+# order matters in vContent - most specific to most general
+>vContent<              := diceExpressionAlone/aliasPlusModifier/aliasPlusExpression/aliasAlone
+
+>verbPhrase<            := '[', !, vContent, ']'
+
+verbPhraseRoot          := ws,verbPhrase
+
+
+
+# you could say:
+# word word *actor word word [verbs]
+# or
+# word word [verbs] word word *actor
+# or
+# word word [verbs] word word
+# or
+# word word [verbs]
+# or
+# [verbs]
+# or
+# word word
+
+words                   := (word,ws)+  
+
+nonCommand              := ?-(command),CHAR*
+
+>sentence<              := command/nonCommand
+sentenceRoot            := ws,sentence
+''') # }}}
+
+# do a half-assed parse with re to eliminate any non-syntax from a person's
+# sentence.
+verbsMaybe = re.compile(r'\[[^]]*\]')
+target = re.compile(r'@[a-zA-Z][a-zA-Z0-9_]*')
+actor = re.compile(r'\*[a-zA-Z][a-zA-Z0-9_]*')
+
+
+class CommandProcessor(disp.DispatchProcessor):
+    commandArgsFound = None
+    commandName = None
+
+    def commandIdentifier(self, (t,s1,s2,sub), buffer):
+        disp.dispatchList(self, sub, buffer)
+        self.commandName = buffer[s1:s2]
+
+    def commandArgs(self, (t,s1,s2,sub), buffer):
+        self.commandArgsFound = buffer[s1:s2]
+
+    def command(self, (t,s1,s2,sub), buffer):
+        disp.dispatchList(self, sub, buffer)
+        return self.commandName, self.commandArgsFound
+
+def parseCommand(s):
+    commandParser = parser.Parser(grammar, root="commandRoot")
+    succ, children, end = commandParser.parse(s, processor=CommandProcessor())
+    if not succ or not end == len(s):
+        raise RuntimeError('%s is not a command' % (s,))
+    return children
+
+
+
+class Sentence(object):
+    def __init__(self):
+        self.diceExpression = None
+        self.command = None
+        self._commandArgs = []
+        self.actor = None
+        self.verbPhrases = []
+        self.targets = []
+
+    def get_commandArgs(self):
+        return self._commandArgs
+
+    def set_commandArgs(self, s):
+        # blah - shlex.split(None) blocks waiting for input, so protect it
+        self._commandArgs = shlex.split(s or '')
+
+    commandArgs = property(get_commandArgs, set_commandArgs)
+
+    def __repr__(self):
+        return '<Sentence %s>' % (str(self),)
+
+    def __str__(self):
+        if self.command:
+            ret = ['.', self.command]
+            for ca in self.commandArgs:
+                ret.append(' ')
+                if ' ' in ca or '\t' in ca:
+                    ca = '"' + ca.replace('"', '\"') + '"'
+                ret.append(ca)
+            return ''.join(ret)
+
+        ret = []
+        a = ret.append
+        if self.actor:
+            a("*%s does " % (self.actor,))
+        if len(self.verbPhrases) == 0:
+            a("***** missing verb phrase *****")
+        else:
+            for verbPhrase in self.verbPhrases:
+                a("[%s]" % (verbPhrase,))
+
+        if self.targets:
+            a(" to @%s" % (self.targets.pop(0),))
+        for target in self.targets:
+            a(" and @%s" % (target,))
+        a(".")
+
+        return ''.join(ret)
+
+class SentenceProcessor(CommandProcessor):
+    def nonCommand(self, (t,s1,s2,sub), buffer):
+        return buffer
+
+def parseSentence(s):
+    sp = SentenceProcessor()
+    sent = Sentence()
+    sentenceParser = parser.Parser(grammar, root="sentenceRoot")
+    succ, children, end = sentenceParser.parse(s, processor=sp)
+    if not succ or not end == len(s):
+        # this might happen if you start with something that looks like a
+        # command but isn't
+        raise RuntimeError('%s is not a sentence' % (s,))
+
+    if sp.commandName:
+        sent.command = sp.commandName
+        sent.commandArgs = sp.commandArgsFound
+        return sent
+    else:
+        actors = actor.findall(children[0])
+        if len(actors) > 1:
+            raise RuntimeError('Too many actors (only one allowed): %s' %
+                    (actors,))
+        elif len(actors) == 1:  
+            sent.actor = actors[0][1:]
+
+        verbCandidates = verbsMaybe.findall(children[0])
+        for vc in verbCandidates:
+            try:
+                sent.verbPhrases.append(parseVerbPhrase(vc))
+            except (RuntimeError, ParserSyntaxError):
+                continue # it's not an error to have something that looks like
+                         # a verb phrase but isn't.  Parse the ones we have,
+                         # and make sure there's at least one ...
+        if len(sent.verbPhrases) == 0:
+            raise RuntimeError('No verb phrase or command')
+
+        for t in target.findall(children[0]):
+            sent.targets.append(t[1:])
+
+        return sent
+
+
+class VerbPhrase(object):
+    dieModifier = None
+    diceExpression = None
+    nonDiceWords = None
+    def __repr__(self):
+        return '<Sentence %s>' % (str(self),)
+
+    def __str__(self):  
+        ret = []
+        a = ret.append
+        if self.nonDiceWords:
+            a(self.nonDiceWords)
+        if self.dieModifier:
+            if self.nonDiceWords:
+                a(' ')
+            a('%+d' % (self.dieModifier,))
+        if self.diceExpression:
+            if self.nonDiceWords:
+                a(' ')
+            a(str(self.diceExpression))
+        return ''.join(ret)
+
+
+
+class VerbPhraseProcessor(disp.DispatchProcessor):
+    def __init__(self, *a, **kw):
+        # disp.DispatchProcessor.__init__(self, *a, **kw)
+        self.verbPhrase = VerbPhrase()
+
+    def diceExpression(self, (t,s1,s2,sub), buffer):
+        self.verbPhrase.diceExpression = diceparser.parseDice(buffer[s1:s2])[0]
+
+    def nonDiceWords(self, (t,s1,s2,sub), buffer):
+        b = buffer[s1:s2]
+        self.verbPhrase.nonDiceWords = ' '.join(b.split()).lower()
+
+    def dieModifier(self, (t,s1,s2,sub), buffer):
+        self.verbPhrase.dieModifier = int(buffer[s1:s2])
+
+def parseVerbPhrase(s):
+    verbPhraseParser = parser.Parser(grammar, root="verbPhraseRoot")
+    proc = VerbPhraseProcessor()
+    succ, children, end = verbPhraseParser.parse(s, processor=proc)
+    if not succ or not end == len(s):
+        raise RuntimeError('%s is not a verb phrase' % (s,))
+    return proc.verbPhrase
+
---
+++ vellumbot/test/test_linesyntax.py
@@ -0,0 +1,100 @@
+import unittest
+
+from ..server import linesyntax
+from simpleparse.error import ParserSyntaxError
+
+
+class LinesyntaxTestCase(unittest.TestCase):
+    """
+    Test that sentences, verbs and commands are parsed successfully
+    """
+    def test_verbPhrase(self):
+        eraise = lambda s, e: self.assertRaises(e, pvp, s)
+        eq = self.assertEqual
+        pvp = lambda s: str(linesyntax.parseVerbPhrase(s))
+        eq(str(pvp("[1d20+1]")),                    "d20+1")
+        eq(pvp("[star]"),                           'star')
+        eq(pvp("[rOck   star]"),                    'rock star')
+        eq(pvp("[woo 1d20+1]"),                     'woo d20+1')
+        eq(pvp("[arr matey 1d20+1x7sort]"),         'arr matey d20+1x7sort')
+        eq(pvp("[woo +2]"),                         'woo +2')
+        eq(pvp("[woo -2]"),                         'woo -2')
+        eraise("[]",                                ParserSyntaxError)
+        eraise("[i am a star",                      ParserSyntaxError)
+        eraise("[1d20+1 1d20+1]",                   ParserSyntaxError)
+        #
+
+    def test_commands(self):
+        _test_commands = [
+        (".hello",                                  [('hello', None)]),
+        (".foo bar",                                [('foo', 'bar')]),
+        (". foo",                                   [('foo', None)]),
+        ("..foo",                                   ParserSyntaxError),
+        ("VellumBot:foo",                           [('foo', None)]),
+        ("velluMBot,foo",                           [('foo', None)]),
+        ("VellumBot foo",                           RuntimeError),
+        ("VellumBot: foo",                          [('foo', None)]),
+        ("velluMBot, foo",                          [('foo', None)]),
+        ("tesTBotfoo",                              RuntimeError),
+        ]
+
+        for tc, expect in _test_commands:
+            if expect in (ParserSyntaxError, RuntimeError):
+                self.assertRaises(expect, linesyntax.parseCommand, tc)
+            else:
+                res = linesyntax.parseCommand(tc)
+                self.assertEqual(res, expect)
+
+    def test_sentences(self):
+        def eqsent(result, assertions):
+            """
+            Inspect the result Sentence against the assertions
+            """
+            if hasattr(assertions, 'items'):
+                for k,v in assertions.items():
+                    self.assertEqual(getattr(result, k), v)
+            elif isinstance(assertions, basestring):
+                self.assertEqual(str(result), assertions)
+            else:
+                assert 0, "BAD TEST"
+
+        errsent = lambda s, e: self.assertRaises(e, sen, s)
+
+        sen = linesyntax.parseSentence
+        eqsent(sen('.gm'),                     {'command': 'gm',
+                                               'commandArgs': []})
+        eqsent(sen('.charname bob bob'),       {'command': 'charname', 
+                                               'commandArgs': ['bob','bob']})
+        eqsent(sen("TestBot, n"),              {'command': 'n',
+                                               'commandArgs': []})
+        eqsent(sen("testbot, n"),              {'command': 'n',
+                                               'commandArgs': []})
+        errsent("testbot n",                   RuntimeError)
+        eqsent(sen(".aliases shara"),          {'command': 'aliases',
+                                               'commandArgs': ['shara']})
+        eqsent(sen(".foobly 'do obly' doo"),   '.foobly "do obly" doo')
+ 
+
+        errsent("lalala",                      RuntimeError)  
+        errsent("*woop1",                      RuntimeError) # missing verb
+        errsent("*jack and *jill [1d20+1]",    RuntimeError) # extra actors
+        eqsent(sen("[attack attack 1d2+10]"),  "[attack attack d2+10].")
+        eqsent(sen("[foo] *woop2"),            "*woop2 does [foo].")
+        eqsent(sen("The [machinegun] being fired at @Shara by the *ninja goes rat-a-tat."),
+                                               "*ninja does [machinegun] to @Shara.")
+        eqsent(sen("*grimlock1 [attack 1d2+10]s the paladin. (@shara)"),
+                                               "*grimlock1 does [attack d2+10] to @shara.")
+        eqsent(sen("I [attack 1d6+1] @grimlock1"),
+                                               "[attack d6+1] to @grimlock1.")
+        eqsent(sen("I [attack -1] @grimlock1"), 
+                                               "[attack -1] to @grimlock1.")
+        eqsent(sen("I [attack +1] @grimlock1"), 
+                                               "[attack +1] to @grimlock1.")
+        eqsent(sen("I [attack 1d6+1x2sort] @grimlock1"), 
+                                               "[attack d6+1x2sort] to @grimlock1.")
+        eqsent(sen("I [cast] a [fireball] @grimlock1 and @grimlock2"),
+                                               "[cast][fireball] to @grimlock1 and @grimlock2.")
+        eqsent(sen("I [cast] a [fireball] @grimlock1 and@grimlock2"),
+                                               "[cast][fireball] to @grimlock1 and @grimlock2.")
+        #
+
