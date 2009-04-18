87eac55249e8c86079238f79004c7f6c2df950f5
Cory Dodt <corymercurial@spam.goonmill.org>
2009-04-18 14:43:24 -0700

    test for cat's grace

--- vellumbot/test/test_reference.py
+++ vellumbot/test/test_reference.py
@@ -9,30 +9,50 @@ class ReferenceTestCase(unittest.TestCase, util.DiffTestCaseMixin):
     Test lookups and references
     """
 
-    def test_find(self):
+    def test_findExactOne(self):
         """
-        Check that the searching of spell database works.
+        Check that the searching of spell database works: exact match one word
         """
         ret = reference.find(u'spell', [u'fireball'])
-        # test exact match one word
         self.assertNoRxDiff(
                 [re.escape(r'<<Fireball>> Evocation || Level: Sorcerer/Wizard 3 || Casting Time: 1 standard action || VSM || Range: Long (400 ft. + 40 ft./level) || Area: 20-ft. -radius spread || Duration: Instantaneous || Save: Reflex half || 1d6 damage per level, 20-ft. radius. || http://www.d20srd.org/srd/spells/fireball.htm'),
                     ],
                 ret,
                 'expected (regex)', 'actual')
-        # test exact match multiple words
+
+    def test_findExactMultiple(self):
+        """
+        Check that the searching of spell database works: exact match multiple
+        words
+        """
         ret = reference.find(u'spell', [u'cure serious wounds mass'])
         self.assertNoRxDiff(
                 [re.escape(r'<<Cure Serious Wounds, Mass>> Conjuration (Healing) || Level: Cleric 7, Druid 8 || Casting Time: 1 standard action || VS || Range: Close (25 ft. + 5 ft./2 levels) || Target: One creature/level, no two of which can be more than 30 ft. apart || Duration: Instantaneous || Save: Will half (harmless) or Will half; see text || Cures 3d8 damage +1/level for many creatures. || http://www.d20srd.org/srd/spells/cureSeriousWoundsMass.htm'),
                     ],
                 ret,
                 'expected (regex)', 'actual')
-        # test non-exact match
+
+    def test_findNonExact(self):
+        """
+        Check that the searching of spell database works: non-exact match
+        """
         ret = reference.find(u'spell', [u'cure'])
         self.assertEqual(len(ret), 5)
         self.assertNoRxDiff(
                 [ur'"cure light wounds mass": \*\*Cure\*\* Light Wounds, Mass Conjuration.*, mass.*\.\.\.', 
                 ur'.*', ur'.*', ur'.*', ur'.*'],
+                ret,
+                'expected (regex)', 'actual')
+
+    def test_findExactSpecialCharacters(self):
+        """
+        Check that the searching of spell database works: match spells with
+        special characters
+        """
+        ret = reference.find(u'spell', [u'cats grace'])
+        self.assertNoRxDiff(
+                [re.escape(r"<<Cat's Grace>> Transmutation || Level: Bard 2, Druid 2, Ranger 2, Sorcerer/Wizard 2 || Casting Time: 1 standard action || VSM || Range: Touch || Target: Creature touched || Duration: 1 min./level || Save: Will negates (harmless) || Subject gains +4 to Dex for 1 min./level. || http://www.d20srd.org/srd/spells/catsGrace.htm"),
+                    ],
                 ret,
                 'expected (regex)', 'actual')
 
