65fcaf0fa4df2707fd310e1fbc0edac4ed0435c0
Cory Dodt <corymercurial@spam.goonmill.org>
2009-04-23 17:12:25 -0700

    test the rest of v.s.alias

--- vellumbot/test/test_alias.py
+++ vellumbot/test/test_alias.py
@@ -3,8 +3,10 @@ from ..server import alias
 from playtools import dice, diceparser
 
 class AliasTestCase(unittest.TestCase):
-
     def test_formatAlias(self):
+        """
+        Used aliases can be displayed as a human-readable result
+        """
         a = 'foobie bletch'
         v1 = ['foo', 'bar']
         v2 = []
@@ -35,6 +37,9 @@ class AliasTestCase(unittest.TestCase):
         self.assertEqual(fmtd, 'foobie bletch, you rolled: 3d6+2 +2 = [3+4+5+2+2 = 16]')
 
     def test_getResult(self):
+        """
+        Dice results can be rolled and their values are accurate
+        """
         aliases = {}
         # junk
         self.assertTrue(alias.getResult('anything', 'foo', aliases) is None)
@@ -57,6 +62,9 @@ class AliasTestCase(unittest.TestCase):
         self.assertEqual(alias.getResult('anything', 'foo', aliases, None, 0)[0].sum(), 5)
 
     def test_shortFormatAliases(self):
+        """
+        Aliases can be formatted into human-readable strings that are short
+        """
         aliases = {'foobar': {('buncha', 'crunch'): '2d20+20',
                               ('yums',): '1234'
                               },
@@ -66,4 +74,55 @@ class AliasTestCase(unittest.TestCase):
                 'buncha crunch=2d20+20, yums=1234'))
         self.assertEqual(alias.shortFormatAliases('empty', aliases), '(none)')
         self.assertEqual(alias.shortFormatAliases('NOBODY', aliases), '(none)')
+
+    def test_resolve(self):
+        """
+        A stringy version of a result or None is returned by resolve
+        """
+        aliases = {}
+        _exp = diceparser.parseDice('1d1')
+        actor = 'GeeEm'
+        verbs = tuple('smack down'.split())
+
+        formatted = alias.resolve(actor, verbs, parsed_dice=_exp, 
+                temp_modifier=2, aliases=aliases)
+        self.assertEqual(formatted, 'GeeEm, you rolled: smack down d1 +2 = [1+2 = 3]')
+
+        _exp = None
+        verbs = tuple('this no Alias iS'.split())
+        formatted = alias.resolve(actor, verbs, parsed_dice = _exp,
+                temp_modifier=None, aliases=aliases)
+        self.assertEqual(formatted, None)
+
+    def test_aliasHooks(self):
+        """
+        We can associate a callable with an alias, and that callable will get
+        called when we roll the dice
+        """
+        aliases = {}
+        res = []
+        _exp = diceparser.parseDice('1d1')
+
+        def smackDownHook(user, rolled):
+            """
+            We take a string for the user who invoked the alias and an object
+            which is the structured result of the roll
+            """
+            res.append((user, rolled[0].sum()))
+
+        actor = 'testing alias hooks'
+        verbs = 'smack down'
+
+        # test that nothing happens when it is not registered..
+        _dontcare = alias.resolve(actor, verbs, parsed_dice=_exp, 
+                temp_modifier=2, aliases=aliases)
+        self.assertEqual(res, [], 
+                "should not have called smackDownHook here but did")
+
+        # now register and do it again
+        alias.registerAliasHook('smack down', smackDownHook)
+        _dontcare = alias.resolve(actor, verbs, parsed_dice=_exp, 
+                temp_modifier=2, aliases=aliases)
+        self.assertEqual(res, [('testing alias hooks', 3)], 
+                "should have called smackDownHook here but did not")
 
